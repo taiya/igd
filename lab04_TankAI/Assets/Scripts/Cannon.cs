25d9880c90239d0748fc5e3345142bc85bacd4f6
r4ghu <raghu_sri95@yahoo.in>
2017-02-20 15:05:52 -0800

    Pushing Lab04

---
+++ lab04_TankAI/Assets/Scripts/Cannon.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+	public Rigidbody2D bulletPrefab;
+
+	public float bulletSpeed = 10;
+
+	AudioSource audio;
+
+	void Start() {
+		audio = GetComponent<AudioSource> ();
+	}
+
+	public void AimToward(Vector3 direction) {
+		transform.up = direction;
+	}
+
+	public void Fire() {
+		
+		var newBullet = Instantiate (bulletPrefab,transform.position,transform.rotation);
+		newBullet.velocity = transform.up * bulletSpeed;
+		newBullet.GetComponent<Bullet> ().owner = GetComponentInParent<Tank> ();
+
+		if (audio != null) {
+
+			// TODO: -------------------------
+			// Play cannon sound
+
+
+
+			// -------------------------------
+			
+		}
+
+	}
+
+}
---
+++ lab04_TankAI/Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour {
+
+	public int numberOfTanks = 6;
+
+	public GameObject tankPrefab;
+
+	void SpawnTank () {
+		
+		var newTank = Instantiate (tankPrefab);
+
+		newTank.transform.position = Random.insideUnitCircle * 10;
+
+	}
+
+	void Update () {
+
+		var tanksList = GameObject.FindObjectsOfType<TankAI> ();
+
+		if (tanksList.Length < numberOfTanks) {
+			SpawnTank ();
+		}
+		
+	}
+}
