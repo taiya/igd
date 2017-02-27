eb5e6d6c8cb4f9ba6be4c63afc9d31ef7daa7fed
r4ghu <raghu_sri95@yahoo.in>
2017-02-27 12:18:57 -0800

    Pushing Lab05

---
+++ lab05_FighterJetFX/Assets/Scripts/AutoDespawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDespawn : MonoBehaviour {
+
+	float spawnTime;
+
+	void Start () {
+		spawnTime = Time.time;
+	}
+
+	void Update () {
+		if (Time.time > spawnTime + 4) {
+			Destroy (this.gameObject);
+		}
+	}
+}
