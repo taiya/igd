2b6e4a09864c77371fb9f7007d473de69b36acdf
r4ghu <raghu_sri95@yahoo.in>
2017-03-13 19:22:43 -0700

    Pushing lab06

---
+++ lab06_MultiplayerTanks/Assets/Scripts/Tank.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour {
+
+	public float moveSpeed = 2;
+	public float turnSpeed = 200;
+
+	public float health = 100;
+
+	public Cannon cannon;
+	public HealthBar healthBar;
+
+	void Start () {}
+
+	public void MoveForward() {
+		transform.position += transform.up * Time.deltaTime * moveSpeed;
+	}
+
+	public void MoveBackward() {
+		transform.position -= transform.up * Time.deltaTime * moveSpeed;
+	}
+
+	public void TurnToward(Vector3 direction) {
+		
+		var axis = Vector3.Cross (direction, transform.up);
+
+		if (axis.magnitude < 0.03) {
+			axis = Vector3.zero;
+		}
+		var sign = -Vector3.Dot (Vector3.forward, axis.normalized);
+
+		transform.RotateAround (transform.position, Vector3.forward, sign * Time.deltaTime * turnSpeed);
+
+	}
+
+	public void ApplyDamage(float damage) {
+		
+		health -= damage;
+
+		healthBar.SetHealth (health);
+
+		if (health <= 0) {
+				
+			health = 100;
+			healthBar.SetHealth (100);
+
+			transform.position = Random.insideUnitCircle * 10;
+
+		}
+
+	}
+
+	void Update() {
+
+		healthBar.SetHealth (health);
+
+	}
+}
