7e7f0df58995b72307f8de780a5155f328d5568e
r4ghu <raghu_sri95@yahoo.in>
2017-03-27 16:45:51 -0700

    Pushing Lab08

---
+++ lab08_RunningMan/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour {
+
+	Animator animator;
+
+	void Start () {
+
+		animator = GetComponent<Animator> ();
+
+	}
+
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.A)) {
+			animator.SetBool("TurningLeft",true);
+		}
+		if (Input.GetKeyUp(KeyCode.A)) {
+			animator.SetBool("TurningLeft",false);
+		}
+
+		if (Input.GetKeyDown(KeyCode.D)) {
+			animator.SetBool("TurningRight",true);
+		}
+		if (Input.GetKeyUp(KeyCode.D)) {
+			animator.SetBool("TurningRight",false);
+		}
+
+		if (Input.GetKeyDown(KeyCode.W)) {
+			animator.SetBool("Running",true);
+		}
+		if (Input.GetKeyUp(KeyCode.W)) {
+			animator.SetBool("Running",false);
+		}
+
+	}
+}
