7b837da5b976193f0e72f4beced5587eefd1bcc9
drebain <drebain@uvic.ca>
2017-10-04 16:18:27 -0700

    Add UI lab

---
+++ UserInterface/Assets/Scripts/QuitButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour {
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+
+}
---
+++ UserInterface/Assets/Scripts/Rotater.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotater : MonoBehaviour {
+
+	public void Rotate(float angle) {
+		transform.Rotate(0.0f, 0.0f, -angle);
+	}
+
+}
